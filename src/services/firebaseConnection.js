1ec0a11d9e12596772312578d5e76fd645dcd7f5
Felipe-Borba <felipe.borba0109@gmail.com>
2022-03-10 23:13:34 -0300

    add fireabase connection

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCf_e3H7WR4k_XY5p6d1ifHWyhR-L-z0SE",
+  authDomain: "sistema-chamados-79184.firebaseapp.com",
+  projectId: "sistema-chamados-79184",
+  storageBucket: "sistema-chamados-79184.appspot.com",
+  messagingSenderId: "130844607175",
+  appId: "1:130844607175:web:caa8d61e6ea2290bc434e2",
+  measurementId: "G-6Q7LTL4S2Y",
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
