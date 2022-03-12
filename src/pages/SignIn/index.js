ebe442919575a88c21a05424dd15c628e8c0348a
Felipe-Borba <felipe.borba0109@gmail.com>
2022-03-11 23:14:33 -0300

    implement signIn page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,46 @@
+import { useState } from "react";
+import "./signIn.css";
+import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
+
 function SignIn() {
-  return <div>index</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert("todo");
+  }
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="logo-area">
+          <img src={logo} alt="Sistema Logo" />
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <h1>Entrar</h1>
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+          />
+          <input
+            type="password"
+            placeholder="******"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+
+          <button type="submit">Acessar</button>
+        </form>
+
+        <Link to="/register">Criar uma Conta</Link>
+      </div>
+    </div>
+  );
 }
 
 export default SignIn;
