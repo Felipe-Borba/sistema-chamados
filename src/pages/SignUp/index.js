0ca98348d7b60a5de14d2cd3c13faedc4cc08aae
Felipe-Borba <felipe.borba0109@gmail.com>
2022-03-13 12:50:06 -0300

    sketch sign up page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,51 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+
 function SignUp() {
-  return <div>SignUp</div>;
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert("todo");
+  }
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="logo-area">
+          <img src={logo} alt="Sistema Logo" />
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <h1>Cadastrar</h1>
+          <input
+            type="text"
+            placeholder="Nome"
+            value={nome}
+            onChange={({ target }) => setNome(target.value)}
+          />
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+          />
+          <input
+            type="password"
+            placeholder="******"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+          <button type="submit">Acessar</button>
+        </form>
+
+        <Link to="/">Já tem uma conta? Entre</Link>
+      </div>
+    </div>
+  );
 }
 
 export default SignUp;
